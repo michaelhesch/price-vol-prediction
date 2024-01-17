51ae7f86123d0704beecf9924cc0b6ac7b7c1893
michaelhesch <michael.hesch@gmail.com>
2024-01-17 23:11:15 +0000

    Add predictive model code and descriptions

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from scipy.stats import probplot, moment
 from numpy.linalg import LinAlgError
 from itertools import product
 from tqdm import tqdm
+from arch import arch_model
 
 warnings.filterwarnings('ignore')
 sns.set_style('whitegrid')
@@ -30,7 +31,8 @@ stock_df = yf.download(tickers='NVDA',
 nvda_log = np.log(stock_df['Adj Close']).diff().dropna()
 
 # Create function to visualize stock data
-# Looking for 
+# Looking for what lags in autocorrelation are most correlated
+# This will help inform the model to predict price by log returns
 def plot_correlogram(x, lags=None, title=None):
     lags = min(10, int(len(x)/5)) if lags is None else lags
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14, 8))
@@ -68,6 +70,75 @@ def plot_correlogram(x, lags=None, title=None):
 
     return
 
+# Visualize based on logs
+# Log results show autocorrelation appears random
 plot_correlogram(x=nvda_log,
                  lags=120,
                  title=None)
+
+# Turn log returns into daily volitility
+nvda_daily_vol = (nvda_log*nvda_log.mean())**2
+
+# Visualize based on daily volitility
+# Now we see more correlation trend using vol approach
+# This will allow a model to reasonably predict next day vol
+plot_correlogram(x=nvda_daily_vol,
+                 lags=120,
+                 title=None)
+
+'''
+Build predictive model
+'''
+# Use 10 years of data, 252 trading days/yr
+trainsize = 10*252
+# Remove outliers from the data which will skew model
+# If upper or lower is higher than specified quantile, assign the quantile as value
+data = nvda_log.clip(lower=nvda_log.quantile(.05),
+                     upper=nvda_log.quantile(.95))
+# Length of entire sample
+T = len(nvda_log)
+
+results = {}
+
+# Loop through the combinations of autoregression model and moving avg and fit models
+for p in range(1, 5):
+    for q in range(1, 5):
+        print(f'{p} | {q}')
+        
+        result = []
+        for s, t in enumerate(range(trainsize, T-1)):
+            train_set = data.iloc[s:t]
+            test_set = data.iloc[t+1]
+            model = arch_model(y=train_set, p=p, q=q).fit(disp='off')
+            forecast = model.forecast(horizon=1)
+            mu = forecast.mean.iloc[-1, 0]
+            var = forecast.variance.iloc[-1, 0]
+            result.append([(test_set-mu)**2, var])
+        df = pd.DataFrame(result, columns=['y_true', 'y_pred'])
+        results[(p, q)] = np.sqrt(mean_squared_error(df.y_true, df.y_pred))
+
+# Display model results, actual and predicted var
+result_df = pd.DataFrame(results, index=pd.Series(0)).unstack()
+print(result_df)
+
+# Select the result with the best fit
+result_df = result_df.idxmin()
+print(result_df)
+
+# run arch model with best p and q model result from step above
+model_data = nvda_log.clip(lower=nvda_log.quantile(.05),
+                     upper=nvda_log.quantile(.95))
+best_model = arch_model(y=model_data,
+                        p=4,
+                        q=2).fit(update_freq=5, disp='off')
+print(best_model.summary())
+
+'''
+Rolling prediction - each day fit model and predict next day var
+Compare current day variance with next day prediction
+Form trading strategy based on this approach
+'''
+# Use log data
+
+# Calculate rolling var
+
