de842a6a05097bf56c9a2e873e291a2b048fe355
michaelhesch <michael.hesch@gmail.com>
2024-01-17 22:35:40 +0000

    Initial commit

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ stock_df = yf.download(tickers='NVDA',
 nvda_log = np.log(stock_df['Adj Close']).diff().dropna()
 
 # Create function to visualize stock data
+# Looking for 
 def plot_correlogram(x, lags=None, title=None):
     lags = min(10, int(len(x)/5)) if lags is None else lags
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14, 8))
@@ -38,23 +39,32 @@ def plot_correlogram(x, lags=None, title=None):
     x.rolling(21).mean().plot(ax=axes[0][0], c='k', lw=1)
     
     q_p = np.max(q_stat(acf(x, nlags=lags), len(x))[1])
+    
     stats = f'Q-Stat: {np.max(q_p):>8.2f}\nADF: {adfuller(x)[1]:>11.2f}'
     
-    axes[0][0].text(x=0.02, y=0.85, s=stats, transform=axes[0][0].transAxes)
+    axes[0][0].text(x=.02, y=.85, s=stats, transform=axes[0][0].transAxes)
     
     probplot(x, plot=axes[0][1])
+    
     mean, var, skew, kurtosis = moment(x, moment=[1, 2, 3, 4])
+    
     s = f'Mean: {mean:>12.2f}\nSD: {np.sqrt(var):>16.2f}\nSkew: {skew:12.2f}\nKurtosis: {kurtosis:9.2f}'
 
-    axes[0][1].text(x=0.02, y=0.75, s=s, transform=axes[0][1].transAxes)
-    plot_acf(x=x, lags=lags, zero=False, ax=axes[1][0])
-    plot_pacf(x, lags=lags, zero=False, ax=axes[1][1])
+    axes[0][1].text(x=.02, 
+                    y=.75, 
+                    s=s,
+                    transform=axes[0][1].transAxes)
+    
+    plot_acf(x=x, lags=lags, zero=False, auto_ylims=True, ax=axes[1][0])
+    plot_pacf(x, lags=lags, zero=False, auto_ylims=True, ax=axes[1][1])
+    
     axes[1][0].set_xlabel('Lag')
     axes[1][1].set_xlabel('Lag')
+    
     fig.suptitle(title, fontsize=14)
     sns.despine()
     fig.tight_layout()
-    fig.subplots_adjust(top=0.9)
+    fig.subplots_adjust(top=.9)
 
     return
 
